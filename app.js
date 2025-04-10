4c63bbc1cfc04505ce9712fefbaa2b054731f001
Durgeshwar-AI <durgesh34shaw94@gmail.com>
2025-04-10 13:27:44 +0530

    New design

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { config } from "dotenv";
+import { fetchWeatherApi } from "openmeteo";
 import path from "path";
 import axios from "axios";
 
@@ -8,7 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const KEY = process.env.KEY;
 const URL = process.env.URL;
-const URLLATLONG= process.env.URLLATLONG;
+const URLLATLONG = process.env.URLLATLONG;
+const urlForecast = "https://api.open-meteo.com/v1/forecast";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,19 +18,85 @@ app.use(express.static(path.join(process.cwd(), "public")));
 app.set("view engine", "ejs");
 
 app.get("/", async (req, res) => {
+  const params = {
+    latitude: 28.6519,
+    longitude: 77.2315,
+    daily: ["uv_index_max", "weathercode"],
+    current: [
+      "temperature_2m",
+      "relative_humidity_2m",
+      "wind_speed_10m",
+      "apparent_temperature",
+    ],
+    forecast_days: 1,
+  };
   try {
-    const response = await axios.get(
-      `${URL}?q=New Delhi&appid=${KEY}&units=metric`
-    );
-    const data = response.data;
+    const responses = await fetchWeatherApi(urlForecast, params);
+    const response = responses[0];
+
+    // Attributes for timezone and location
+    const utcOffsetSeconds = response.utcOffsetSeconds();
+    const timezone = response.timezone();
+    const timezoneAbbreviation = response.timezoneAbbreviation();
+    const latitude = response.latitude();
+    const longitude = response.longitude();
+    const current = response.current();
+    const daily = response.daily();
+
+    const categorizeUV = (uv) => {
+      if (uv <= 2) return "Low";
+      if (uv <= 5) return "Medium";
+      if (uv <= 7) return "High";
+      if (uv <= 10) return "Very High";
+      return "Extreme";
+    };
+
+    const categorizeWeather = (weather)=>{
+      switch (weather) {
+        case 0: return "Clear Sky";
+        case 1: return "Mainly clear";
+        case 2: return "Partly cloudy";
+        case 3: return "Overcast";
+        case 45:
+        case 48: return "Fog";
+        case 51:
+        case 53:
+        case 55: return "Light drizzle";
+        case 56:
+        case 57: return "Freezing drizzle";
+        case 61:
+        case 63:
+        case 65: return "Rain";
+        case 66:
+        case 67: return "Freezing rain";
+        case 71:
+        case 73:
+        case 75: return "Snow fall";
+        case 77: return "Snow grains";
+        case 80:
+        case 81:
+        case 82: return "Rain showers";
+        case 85:
+        case 86: return "Snow Showers";
+        case 95: return "Thunderstorm";
+        case 96:
+        case 99: return "Thunderstorm with hail";
+        default: return "Unknown";
+      }
+    }
 
     const weatherData = {
-      city: data.name,
-      temperature: `${data.main.temp}`,
-      humidity: `${data.main.humidity}`,
-      windSpeed: `${data.wind.speed}`,
-      icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
-      type: data.weather[0].main
+      current: {
+        time: new Date((Number(current.time()) + utcOffsetSeconds) * 1000),
+        temperature: current.variables(0).value().toFixed(2),
+        humidity: current.variables(1).value(),
+        wind: current.variables(2).value().toFixed(2),
+        apparentTemperature: current.variables(3).value().toFixed(2),
+      },
+      daily: {
+        uv: categorizeUV(daily.variables(0).valuesArray()),
+        type : categorizeWeather(daily.variables(1).valuesArray()[0])
+      },
     };
 
     res.render("weather", { weather: weatherData });
@@ -44,6 +112,16 @@ app.get("/", async (req, res) => {
       },
     });
   }
+});
+
+app.get("/api/city", async (req, res) => {
+  const response = await axios.get(
+    `https://geocoding-api.open-meteo.com/v1/search?name=${req.query.name}&count=5&language=en&format=json`
+  );
+  const data = response.data;
+
+  console.log(data);
+  res.json(data);
 });
 
 app.post("/api/v1/weather", async (req, res) => {
@@ -77,7 +155,6 @@ app.post("/api/v1/latweather", async (req, res) => {
     res.status(500).json({ error: "Failed to fetch weather data" });
   }
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
